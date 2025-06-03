cc31bcf920297607e75b85f257cd3568f6dbf69d
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-06-02 23:16:29 -0300

    Adição atividade1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//Página 20 - Converta galões em litros
+public class Main {
+    public static void main(String[] args) {
+        double galao, litros;
+
+        galao = 5;
+        litros = galao * 3.7854;
+
+        System.out.println(litros);
+    }
+}
