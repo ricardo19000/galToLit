6c83d85f237bbe262fd8b90e6ae50869c21fb8d4
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-06-02 23:49:33 -0300

    Adição de tabela de 1 a 100 com for loop

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-//Página 20 - Converta galões em litros
+//Página 20 - Converta galões em litros + tabela com for loop
 public class Main {
     public static void main(String[] args) {
-        double galao, litros;
+        double litros;
+        int galao, contador;
 
-        galao = 5;
-        litros = galao * 3.7854;
+        contador = 0; //Configura o contador ao valor inicial de zero
+        for(galao = 1; galao <= 100; galao++) {
+            litros = galao * 3.7854;
+            System.out.println(galao + " Galões é igual a " + litros + " Litros");
 
-        System.out.println(litros);
+            contador++; //Soma +1 ao contador no final do código
+            if(contador == 10) {
+                System.out.println(); //Imprime uma linha em branco quando o contador for igual a 10
+                contador = 0; //Retorna o contador ao valor de 0
+            }
+        }
     }
 }
